72b7d72803ba7fded49dfefb64c4a18b47a66539
Thijs <thijs0302@gmail.com>
2023-11-24 02:52:16 +0100

    fixed light loop

--- misc/js/background.js
+++ misc/js/background.js
@@ -1,19 +1,16 @@
 var MR = function (X) { return Math.random() * X }, TwL = TweenLite, G = document.querySelectorAll('.crystal');
 
 function BTweens() {
-    var W = window.innerWidth, H = window.innerHeight, C = 40;
-    TwL.killDelayedCallsTo(BTweens); 
-    TwL.delayedCall(C * 4, BTweens);
+    var W = window.innerWidth, H = window.innerHeight, C = 4000;
     for (var i = G.length; i--;) {
         var c = C, BA = [], GWidth = G[i].offsetWidth, GHeight = G[i].offsetHeight;
         while (c--) { 
             var SO = MR(1); 
             var distanceFactor = 0.8; // Adjust this factor to control the distance of movement
             BA.push({ x: MR((W - GWidth) * distanceFactor), y: MR((H - GHeight) * distanceFactor)});
-            
         }
         if (G[i].T) { 
-            G[i].T.kill(); 
+            BTweens();
         }
         G[i].T = TweenMax.to(G[i], C * 2, { bezier: { timeResolution: 0, type: "soft", values: BA }, delay: i * 0.35, ease: Linear.easeNone });
     }
@@ -22,14 +19,7 @@ function BTweens() {
 // Call the function initially
 BTweens();
 
-// Update the function for window resize
-window.onresize = function () {
-    TwL.killDelayedCallsTo(BTweens); 
-    TwL.delayedCall(0.4, BTweens);
-};
-
-// Update the function for scroll
-window.onscroll = function () {
-    TwL.killDelayedCallsTo(BTweens); 
-    TwL.delayedCall(0.4, BTweens);
+// Update the function for window resize and scroll
+window.onresize = window.onscroll = function () {
+    BTweens();
 };
