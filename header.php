b191be85aa9d63a78a6749b5f6280e67a3204f5d
Thijs <thijs0302@gmail.com>
2023-12-20 11:43:33 +0100

    google analythics

--- header.php
+++ header.php
@@ -12,6 +12,16 @@
   <title>Thijs Rietveld</title>
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HMYHRQBSLZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HMYHRQBSLZ');
+</script>
+
 <body>
 
 <div class="bg-animation">
